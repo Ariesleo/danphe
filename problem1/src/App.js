69e0256fd7e95232e4169097118081bfad6d44b8
Murari Gupta <alexgupta2367@gmail.com>
2023-06-01 22:12:17 +0545

    implemented the button component and changed the color randomly

--- problem1/src/App.js
+++ problem1/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import { useAppContext } from './context/appContext';
 
 function App() {
+  const { changeButtonColor } = useAppContext();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button handleClick={changeButtonColor} />
       </header>
     </div>
   );
